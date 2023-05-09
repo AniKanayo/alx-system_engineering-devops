667eeea5ba63b95803ccf8eefa999732c37c0020
AniKanayo <micanian4real@gmail.com>
2023-05-09 21:37:33 +0000

    api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" python3 -c 'print(__import__("requests").__doc__)'"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "myBot/0.0.1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
