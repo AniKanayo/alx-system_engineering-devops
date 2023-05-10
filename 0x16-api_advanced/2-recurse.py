fef873f604fca0c3e3ce7f4da45b011e806c913a
AniKanayo <micanian4real@gmail.com>
2023-05-10 01:27:58 +0000

    api_advanced

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A recursive function that queries the Reddit API and returns a list"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for d in results.get("children"):
+        hot_list.append(d.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
