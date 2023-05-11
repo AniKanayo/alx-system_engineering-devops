0c563f3ec78a50d3511687a54ea0d35e1fab96e1
AniKanayo <micanian4real@gmail.com>
2023-05-11 04:17:51 +0000

    Webstack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using Puppet automation to fix 500 error in a web server.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
